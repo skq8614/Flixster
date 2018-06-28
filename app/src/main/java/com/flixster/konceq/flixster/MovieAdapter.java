0d71dabee29ea8a78256dcb71dafc764febf0045
Konce Quispe <konceq@fb.com>
2018-06-27 17:11:53 -0700

    Initial commit

---
+++ app/src/main/java/com/flixster/konceq/flixster/MovieAdapter.java
@@ -0,0 +1,72 @@
+package com.flixster.konceq.flixster;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.flixster.konceq.flixster.models.Movie;
+
+import java.util.ArrayList;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+
+    //list of movies
+    ArrayList<Movie> movies;
+
+    public MovieAdapter(ArrayList<Movie> movies){
+        this.movies = movies;
+    }
+
+    //create the viewholder as a static inner class
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        //track view objects
+        ImageView ivPosterImage;
+        TextView tvTitle;
+        TextView tvOverview;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            //lookup view objects by id
+            ivPosterImage = itemView.findViewById(R.id.ivPosterImage);
+            tvOverview = itemView.findViewById(R.id.tvOverview);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+        }
+    }
+
+    //creates and inflates a new view
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //get the context and create the inflater
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        //create the view using the item_movie layout
+        View movieView = inflater.inflate(R.layout.item_movie, parent, false);
+        //return a new ViewHolder
+        return new ViewHolder(movieView);
+    }
+
+    //binds an inflated view to a new item
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        //get the movie data at the specified position
+        Movie movie = movies.get(position);
+        //populate the view with the movie data
+        holder.tvTitle.setText(movie.getTitle());
+        holder.tvOverview.setText(movie.getOverview());
+
+        //TODO - set image using Glide
+    }
+
+    //returns the total number of items in the list
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+}
